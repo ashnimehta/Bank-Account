fdb3f1ee50a23b053b8f6ce26188b3463249da9a
Ariel Ahdoot <arieli612@aol.com>
2015-11-20 14:07:03 -0500

    added printlist function, added function names to bankAccount.h file

--- bankAccount.c
+++ bankAccount.c
@@ -6,8 +6,12 @@ int num_acc;
 int i;
 
 void freeBankAcc(BankAccount ba){
+	if(!ba){
+		//error, void BankAccount pointer
+	}
 	free(ba->name);
 	free(ba);
+	return;
 }
 
 void freeBankArr(){
@@ -15,11 +19,26 @@ void freeBankArr(){
 		freeBankAcc(bankArray[i]);
 	}
 	free(bankArray);
+	return;
 }
 
 void initArray(){
 	bankArray = malloc(MAX_ACCOUNTS * sizeof (BankAccount));
 	num_acc = 0;
+	return;
+}
+
+void printList(){
+	for(i=0;i<num_acc;i++){
+		printf("%s,%d"bankArray[i]->name,bankArray[i]->balance);
+		if(bankArray[i]->isf == 1){
+			printf(",IN SERVICE\n");
+		}
+		else{
+			printf("\n");
+		}
+	}
+	return;
 }
 
 BankAccount initBankAccount(string accname){
--- bankAccount.h
+++ bankAccount.h
@@ -1,1 +1,7 @@
 #include "utils.h"
+
+void freeBankAcc(BankAccount ba);
+void freeBankArr();
+void initArray();
+BankAccount initBankAccount(string accname);
+void printList();
