1b5017c3087f6cecf5bf3c903d0828d4298e0896
Ashni Mehta <ashnimehta@gmail.com>
2015-12-05 22:23:39 -0500

    julia made us go to the kitchen

--- client.h
+++ client.h
@@ -1,12 +1,11 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include "client.h"
+#include "utils.h"
 
 void* c_input (void* in);
 void* s_output(void* out);
 int serverconnect(char* server, char* port);
-int main(int argc, char** argv);
 void spawn_threads(int sd);
 
 #endif
